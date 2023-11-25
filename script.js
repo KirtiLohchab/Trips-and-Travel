388becf075620e8716b617d7f2e8b052546b21d1
kirtiahlawat <ahlawatkirti227@gmail.com>
2023-11-25 15:53:21 +0530

    date update in script

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 function bookNow() {
   var form = document.getElementById("bookingForm");
 
-  // Additional validation for start date and end date
+  // Additional validation for start date, end date, and future date
   var startDate = new Date(document.getElementById("startDate").value);
   var endDate = new Date(document.getElementById("endDate").value);
+  var currentDate = new Date();
 
-  if (form.checkValidity() && startDate < endDate) {
+  if (
+    form.checkValidity() &&
+    startDate < endDate &&
+    startDate >= currentDate &&
+    endDate >= currentDate
+  ) {
     alert(" Thankyou your Booking is successful!");
   } else {
     form.classList.add("was-validated");
+
     if (startDate >= endDate) {
       alert("Start date must be less than end date.");
+    }
+
+    if (startDate < currentDate) {
+      alert("Start date must be in the future.");
+    }
+
+    if (endDate < currentDate) {
+      alert("End date must be in the future.");
     }
   }
 }
